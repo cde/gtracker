68d2b627a64afd61004ec5e9b6f47bf04ee33627
Carmen Diaz <cdechauri@gmail.com>
2018-10-11 02:43:30 -0700

    Create User model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,54 @@
 //Authentication
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs')
+
+const User = require("../../models/User")
+
+// @route GET api/users/register
+// @desc Register user
+// @access public
 
 router.get('/', (req, res) => res.json({
     msg: 'It works'
 }))
+
+// @route POST api/users/create
+// @desc Register user
+// @access public
+
+router.post('/create', (req, res) => {
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: 'Email already exists' });
+        } else {
+            const avatar = gravatar.url(req.body.email, {
+                s: '200', // Size
+                r: 'pg', // Rating
+                d: 'mm' // Default
+            });
+
+            const newUser = new User({
+                firstName: req.body.first_name,
+                lastName: req.body.last_name,
+                email: req.body.email,
+                profile: avatar,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
 
 module.exports = router;
