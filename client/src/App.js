32ef4d7eb246b3d5ba0cedef431f8d287b9422f8
Carmen Diaz <cdechauri@gmail.com>
2018-10-27 06:25:08 -0700

    Add redux (trying)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/Signup";
 
+import './App.css';
+
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div className="App">
-            <Navbar/>
-              <Switch>
-                  <Route exact path="/" component={Landing} />
-                  <Route exact path="/signup" component={SignUp}/>
-                  <Route exact path="/login" component={Login}/>
-              </Switch>
-            <Footer/>
-          </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+              <div className="App">
+                <Navbar/>
+                  <Switch>
+                      <Route exact path="/" component={Landing} />
+                      <Route exact path="/signup" component={SignUp}/>
+                      <Route exact path="/login" component={Login}/>
+                  </Switch>
+                <Footer/>
+              </div>
+            </Router>
+      </Provider>
     );
   }
 }
